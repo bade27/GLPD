f51297e217abc802cff61b8981acdf93c76414ed
Matteo Badenchini <37813897+bade27@users.noreply.github.com>
2022-07-13 20:14:13 +0200

    gitignore

--- project/main.py
+++ project/main.py
@@ -150,8 +150,8 @@ if __name__ == '__main__':
         
           name = str(i)
         
-          save_petri_net_to_img(net, im, fm, os.path.join(inference_dir, name))
-          save_petri_net_to_pnml(net, im, fm, os.path.join(inference_dir, name))
+          save_petri_net_to_img(net, im, fm, os.path.join(inference_dir, name + '.png'))
+          save_petri_net_to_pnml(net, im, fm, os.path.join(inference_dir, name + '.pnml'))
         
         print(f'{sound_nets}/{len(test)}')
         print(f'{connected}/{len(test)}')
